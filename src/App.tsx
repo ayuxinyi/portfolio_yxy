c77cfaf28e177da826cbec3ba7dd655c2bdcab5d
陈无敌 <yxy_js_code@163.com>
2025-12-29 08:31:47 +0800

    feat: 添加导航栏组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { HeroSection } from "./components/sections/hero-section";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ShowcaseSection } from "./components/sections/showcase-section";
+import { Navbar } from "./components/app/navbar";
 
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <HeroSection />
       <ShowcaseSection />
     </div>
